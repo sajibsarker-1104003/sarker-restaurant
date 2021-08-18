79bc21923771bc0cf9b14b23b0b57536a268f622
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-08-19 00:00:32 +0600

    Preparing Project For Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import MainComponent from './components/MainComponent';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <MainComponent/>
+      </BrowserRouter>
+      
     </div>
   );
 }
