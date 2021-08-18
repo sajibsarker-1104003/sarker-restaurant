66a570f3befd49dcb872610710e893b68e3bb26e
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-08-19 01:20:23 +0600

    Some Changes In Menu Component

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -2,14 +2,27 @@ import React,{Component} from 'react';
 import DISHES from '../../data/dishes';
 import MenuItem from './MenuItem';
 import DishDetail from './DishDetail';
+import {CardColumns,Modal,ModalBody,ModalFooter,Button} from 'reactstrap';
 class Menu extends Component {
   state={
     dishes:DISHES,
-    selectedDish:null
+    selectedDish:null,
+    modalOpen:false
   }
   onDishSelect=dish=>{
-    console.log(dish);
-    this.setState({selectedDish:dish});
+    //console.log(dish);
+    this.setState({
+      selectedDish:dish,
+      modalOpen:!this.state.modalOpen
+
+    });
+     //this.toggleModal();
+  }
+  toggleModal=()=>{
+    this.setState({
+      modalOpen:!this.state.modalOpen
+    })
+    
   }
  render(){
    const menu=this.state.dishes.map(item =>{
@@ -28,12 +41,18 @@ class Menu extends Component {
    return(
     <div className="container">
       <div className="row">
-         <div className="col-6">
-          {menu}
-         </div>
-         <div className="col-6">
-          {dishDetail}
-         </div>
+         <CardColumns>
+           {menu}
+         </CardColumns>
+         <Modal isOpen={this.state.modalOpen} onClick={this.toggleModal}>
+           <ModalBody>
+             {dishDetail}
+           </ModalBody>
+           <ModalFooter>
+             <Button color="secondary" onClick={this.toggleModal}>Close</Button>
+           </ModalFooter>
+
+         </Modal>
       </div>
     </div>
     );
