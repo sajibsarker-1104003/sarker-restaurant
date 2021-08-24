e866a379286288891cc4716bb36d2304a6928e25
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-08-24 21:06:05 +0600

    Applying Redux:Dispatching Action From Component

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -11,7 +11,17 @@ class Home extends Component {
   componentDidMount(){
     console.log("Home State:",this.state);
     console.log("Home Props:",this.props);
+
+    this.props.dispatch({
+      type:'TEST',
+      str:"Sarker"
+    })
   }
+  componentDidUpdate(){
+    console.log("Home Props Updated:",this.props);
+  }
+
+  
   render(){
     document.title="Sarker's Restaurant";
     return(
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,8 +5,17 @@ import COMMENTS from "../data/comments";
 const initialState={
 
   dishes:DISHES,
-  comments:COMMENTS
+  comments:COMMENTS,
+  sample:"Helloo"
 }
 export const Reducer=(state=initialState,action)=>{
+  console.log("From Reducer:",action);
+  if(action.type==='TEST'){
+    return{
+      ...state,
+      sample:action.str
+
+    }
+  }
   return state;
 }
